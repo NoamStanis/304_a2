1ca6a396fb6ae27ed4c870f69dc842914c782ca9
NoamStanislawskiWM <ncstanislawski@email.wm.edu>
2021-02-28 17:21:23 -0500

    commit 2

---
+++ cmake-build-debug/CMakeFiles/A2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "A2"
+  "A2.pdb"
+  "CMakeFiles/A2.dir/acc.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

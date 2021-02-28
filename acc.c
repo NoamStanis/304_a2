34d3a4ef6c2cced799de5ceb074b6c19a3c2a648
NoamStanislawskiWM <ncstanislawski@email.wm.edu>
2021-02-28 18:34:08 -0500

    Can edit values a little bit

--- acc.c
+++ acc.c
@@ -3,11 +3,26 @@
 #include <ctype.h>
 
 
-char mode[3] = "Dec";
+char value_mode[4] = "Dec";
 int dec_value,hex_value,oct_value = 0;
 
-// read in numeric value in mode; return value
+// return the correct value given the mode
 short get_operand(char mode) {
+    int *og_value;
+    switch(mode) {
+
+        case 'O':
+            og_value = &oct_value;
+            break;
+
+        case 'H':
+            og_value = &hex_value;
+            break;
+
+        case 'D':
+            og_value = &dec_value;
+            break;
+    }
     return 0;
 }
 
@@ -19,13 +34,14 @@ void print_acc(short acc, char mode) {
 // print menu; get option until valid; return option
 char print_menu() {
     char option;
+    int *value;
     char valid_options[12] = {'O','o','H','h','D','d','C','c','S','s','Q','q'};
     printf("\n****************************************\n"
            "* Accumulator:         Input Mode: %.3s *\n"
            "* Hex : %.4x                           *\n"
            "* Octal : %.6o                       *\n"
            "* Decimal : %u                          *\n"
-           "****************************************\n", mode,hex_value,oct_value,dec_value);
+           "****************************************\n", value_mode, hex_value, oct_value, dec_value);
 
     printf("\nPlease select one of the following options: \n");
     printf("\nO Octal Mode\n"
@@ -42,19 +58,19 @@ char print_menu() {
     switch (upper_option) {
         case 'O':
             printf("\nMode is Octal");
-            strncpy(mode,"Oct",3);
+            strncpy(value_mode, "Oct", 3);
             print_menu();
             break;
 
         case 'H':
             printf("\nMode is Hexadecimal");
-            strncpy(mode,"Hex",3);
+            strncpy(value_mode, "Hex", 3);
             print_menu();
             break;
 
         case 'D':
             printf("\nMode is Decimal");
-            strncpy(mode,"Dec",3);
+            strncpy(value_mode, "Dec", 3);
             print_menu();
             break;
 
@@ -67,6 +83,24 @@ char print_menu() {
             break;
 
         case 'S':
+            printf("Set the value in the given mode: ");
+            int new_value;
+            scanf("%i",&new_value);
+
+            if (strcmp(value_mode, "Oct") == 0) {
+                oct_value = new_value;
+            }
+
+            if (strcmp(value_mode, "Dec") == 0) {
+                dec_value = new_value;
+            }
+
+            if (strcmp(value_mode, "Hex") == 0) {
+                hex_value = new_value;
+            }
+
+            get_operand(value_mode);
+            print_menu();
             break;
 
         case 'Q':

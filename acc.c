5ccdcdc8c6d8352840b44b3dc1164b6d95a1aa80
NoamStanislawskiWM <ncstanislawski@email.wm.edu>
2021-02-28 22:13:48 -0500

    Final commit!!

--- acc.c
+++ acc.c
@@ -3,31 +3,116 @@
 #include <ctype.h>
 
 
-char value_mode[4] = "Dec";
+char value_mode = 'd';
 short dec_value,hex_value,oct_value = 0;
 short actual_value = 0;
-
-// return the correct value given the mode
-short get_operand(char mode) {
-    return 0;
-}
+short quit = 0;
 
 //print out accumulator values
 void print_acc(short acc, char mode) {
+    char value_str_mode[4];
+    switch(mode){
 
-}
+        case 'D':
+        case 'd':
+            strcpy(value_str_mode,"Dec");
+            break;
 
-// print menu; get option until valid; return option
-char print_menu() {
-    char option;
-    int *value;
-    char valid_options[12] = {'O','o','H','h','D','d','C','c','S','s','Q','q'};
+        case 'O':
+        case 'o':
+            strcpy(value_str_mode,"Oct");
+            break;
+
+        case 'H':
+        case 'h':
+            strcpy(value_str_mode, "Hex");
+            break;
+
+    }
     printf("\n****************************************\n"
            "* Accumulator:         Input Mode: %.3s *\n"
            "*   Hex     :  %04hX                    *\n"
            "*   Octal   :  %06ho                  *\n"
            "*   Decimal :  %-6hd                  *\n"
-           "****************************************\n", value_mode, actual_value, actual_value, actual_value);
+           "****************************************\n", value_str_mode, acc, acc, acc);
+}
+
+// return the correct value given the mode
+short get_operand(char mode) {
+    short new_value; // stores user given value
+    if(mode == 0 ) {
+        return 0;
+    }
+    switch (mode) {
+
+        case 'o':
+        case 'O':
+            printf("Mode is Octal\n");
+            value_mode = 'o';
+            print_acc(actual_value, value_mode);
+            break;
+
+        case 'h':
+        case 'H':
+            printf("Mode is Hexadecimal\n");
+            value_mode = 'h';
+            print_acc(actual_value, value_mode);
+            break;
+
+        case 'd':
+        case 'D':
+            printf("Mode is Decimal\n");
+            value_mode = 'd';
+            print_acc(actual_value, mode);
+            break;
+
+        case 'c':
+        case 'C':
+            actual_value = 0;
+            print_acc(actual_value,value_mode);
+            break;
+
+        case 's':
+        case 'S':
+            if (value_mode == 'o' || value_mode == 'O') {
+                printf("Enter octal value: ");
+                scanf("%ho",&new_value);
+                printf("%ho\n",new_value);
+                actual_value = new_value;
+            }
+
+            if (value_mode == 'd' || value_mode == 'D') {
+                printf("Enter decimal value: ");
+                scanf("%hd",&new_value);
+                printf("%hd\n",new_value);
+                actual_value = new_value;
+            }
+
+            if (value_mode == 'h' || value_mode == 'H') {
+                printf("Enter hex value: ");
+                scanf("%hX", &new_value);
+                printf("%hX\n", new_value);
+                actual_value = new_value;
+            }
+
+            print_acc(actual_value, value_mode);
+            break;
+
+        case 'q':
+        case 'Q':
+            quit = 1;
+            break;
+
+        default:
+            printf("\nInvalid option: %c\n", mode);
+            break;
+    }
+    return 0;
+}
+
+// print menu; get option until valid; return option
+char print_menu() {
+    char option[10];
 
     printf("\nPlease select one of the following options: \n");
     printf("\nO  Octal Mode\n"
@@ -39,71 +124,25 @@ char print_menu() {
 
     printf("Option: ");
 
-    scanf(" %c", &option);
-    printf("%c\n", option);
-    char upper_option = toupper(option);
+    scanf(" %s", option);
+    //printf("%s\n", option);
     short new_value;
-    switch (upper_option) {
-        case 'O':
-            printf("Mode is Octal\n");
-            strncpy(value_mode, "Oct", 3);
-            print_menu();
-            break;
 
-        case 'H':
-            printf("Mode is Hexadecimal\n");
-            strncpy(value_mode, "Hex", 3);
-            print_menu();
-            break;
+    printf("%s\n",option);
 
-        case 'D':
-            printf("Mode is Decimal\n");
-            strncpy(value_mode, "Dec", 3);
-            print_menu();
-            break;
-
-        case 'C':
-            actual_value = 0;
-            print_menu();
-            break;
-
-        case 'S':
-
-            if (strcmp(value_mode, "Oct") == 0) {
-                printf("Enter octal value: ");
-                scanf("%ho",&new_value);
-                printf("%ho\n",new_value);
-                actual_value = new_value;
-            }
-
-            if (strcmp(value_mode, "Dec") == 0) {
-                printf("Enter decimal value: ");
-                scanf("%hd",&new_value);
-                printf("%hd\n",new_value);
-                actual_value = new_value;
-            }
-
-            if (strcmp(value_mode, "Hex") == 0) {
-                printf("Enter hex value: ");
-                scanf("%hx\n",&new_value);
-                actual_value = new_value;
-            }
-
-            print_menu();
-            break;
-
-        case 'Q':
-            break;
-
-        default:
-            printf("\nan invalid option has been entered.\n");
-            print_menu();
-            break;
+    if(option[1] != 0) {
+        printf("\nInvalid option: %s\n", option);
+        return 0;
     }
 
-    return option;
+    char real_option = option[0];
+    return real_option;
 }
 
 int main() {
-    print_menu();
+    print_acc(actual_value,value_mode);
+    while(!quit) {
+        get_operand(print_menu());
+
+    }
 }
